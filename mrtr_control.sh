3f09899663667e61914c62217eb1662d795ba374
xf0r3m <jakubstasinski@protonmail.com>
2023-03-18 18:35:16 +0100

    Dodanie prototypu skryptu mrtr - mrtr_control.sh

--- mrtr_control.sh
+++ mrtr_control.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+function send() {
+  scp $1 pi@mrtr.local:/home/pi;
+}
+
+function copy() {
+  ssh pi@mrtr.local "sudo cp -vv $1 $2";
+}
+
 if [ "$1" ]; then
   case "$1" in
     'passwd') stty -echo;
@@ -11,16 +19,16 @@ if [ "$1" ]; then
               echo;
               if [ "$password" = "$vpasswd" ]; then
                 stty echo; 
-                ssh pi@raspberrypi.local "echo pi:${password} | sudo chpasswd";
+                ssh pi@mrtr.local "echo pi:${password} | sudo chpasswd";
                 if [ $? -eq 0 ]; then echo "Password changed succesfully"; fi
               else
                 echo "Password not verified. Refuse to change.";
                 exit 1;
               fi;;
-    'netsum') ssh pi@raspberrypi.local "sudo /usr/local/bin/netsum -o stdout";;
+    'netsum') ssh pi@mrtr.local "sudo /usr/local/bin/netsum -o stdout";;
     'openvpn') if [ "$2" ]; then
                   case "$2" in
-                    'nodes') ssh pi@raspberrypi.local "wget -q https://vpn.morketsmerke.org/nodes.txt -O /tmp/vpn_nodes.txt; cat /tmp/vpn_nodes.txt;";;
+                    'nodes') ssh pi@mrtr.local "wget -q https://vpn.morketsmerke.org/nodes.txt -O /tmp/vpn_nodes.txt; cat /tmp/vpn_nodes.txt;";;
                     'config') if [ "$3" ]; then
                                 node=$3; 
                                 if echo $node | grep -q '/etc/openvpn/client'; then
@@ -53,12 +61,55 @@ if [ "$1" ]; then
                               else
                                 help; exit 1;
                               fi;
+                              cat > /tmp/client.conf <<EOF
+dev tun
+client
+remote $node
+proto udp
+port 17003
+nobind
+ca $cacert
+cert $certfile
+key $keyfile
+verb 3
+EOF
+                              send /tmp/client.conf;
+                              copy /home/pi/client.conf /etc/openvpn/client/client.conf;;
+                    'connect') ssh pi@mrtr.local "cd /etc/openvpn/client; sudo openvpn --daemon --config client.conf";;
+                    'certs') if [ "$3" ]; then
+                              filename=$(basename $3);
+                              send $3;
+                              ssh pi@mrtr.local "sudo tar -xzvf /home/pi/$filename -C /etc/openvpn/client";
+                             else
+                              help;
+                              exit 1;
+                             fi;;
+                    'pkp') echo "There is no echo, you are on your own. Good luck"; 
+                          stty -echo;
+                          ssh pi@mrtr.local "sudo systemd-tty-ask-password-agent";
+                          stty echo;;
+                    'route') 
                   esac
                 else
                   help;
                   exit 1;
                fi;;
-               
+    'wlan') if [ "$2" ]; then
+              case $2 in
+                'list') ssh pi@mrtr.local "sudo wlanconn list";;
+                'connect') if [ "$4" ]; then
+                            ssh pi@mrtr.local "sudo wlanconn \"$3\" $4";
+                           else
+                            ssh pi@mrtr.local "sudo wlanconn \"$3\"";
+                           fi;;
+              esac
+            else
+              help;
+              exit 1;
+            fi;;
+    'shell') ssh pi@mrtr;
+    'reboot') ssh pi@mrtr.local "sudo reboot";;
+    'poweroff') ssh pi@mrtr.local "sudo poweroff";;         
   esac
 else
   help;
